d56bbd4cebe4465734c203d54ee1cd31b47efd77
Snehal Patil <snehal.patil.1303@knorex.com>
2020-08-16 00:54:08 +0530

    updated construction page

--- js/index.js
+++ js/index.js
@@ -5,22 +5,22 @@ particlesJS.load('particles-js', 'assets/particles.json', function() {
 
   /*** Countdown Timer */
   $("#days")
-  .countdown("2019/10/27", function (event) {
+  .countdown("2020/10/04", function (event) {
       $(this).html(event.strftime('%D <em>days</em>'))
   });
 
   $("#hours")
-  .countdown("2019/10/27", function (event) {
+  .countdown("2020/10/04", function (event) {
       $(this).html(event.strftime('%H <em>hours</em>'))
   });
 
   $("#minutes")
-  .countdown("2019/10/27", function (event) {
+  .countdown("2020/10/04", function (event) {
       $(this).html(event.strftime('%M <em>minutes</em>'))
   });
 
   $("#seconds")
-  .countdown("2019/10/27", function (event) {
+  .countdown("2020/10/04", function (event) {
       $(this).html(event.strftime('%S <em>seconds</em>'))
   });
 
